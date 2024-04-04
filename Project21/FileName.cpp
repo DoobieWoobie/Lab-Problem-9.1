985f0bf8e19e82179a167fa6e9e4eae00aa1c03d
Andrew Wu <a-wu.1@onu.edu>
2024-04-04 09:35:50 -0400

    Completed order of appearance array

--- Project21/FileName.cpp
+++ Project21/FileName.cpp
@@ -1,1 +1,14 @@
-a
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	srand(time(0));
+	int randInt[10];
+	for (int i = 0; i < 10; i++)
+	{
+		randInt[i] = rand() % 9999;
+		cout << randInt[i] << "	  ";
+	}
+}
